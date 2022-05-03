9816e730617e3a7121e07849178b95579408ea2e
Masayoshi Kojima <james@jws.work>
2022-05-03 11:39:37 +0900

    add 26

---
+++ 26-code-structuring-for-bigger-projects/src/Experience/World/World.js
@@ -0,0 +1,29 @@
+import Experience from '../Experience';
+import Environment from './Environment';
+import Floor from './Floor';
+import Fox from './Fox';
+
+export default class World {
+  constructor() {
+    this.experience = new Experience();
+    this.scene = this.experience.scene;
+    this.resources = this.experience.resources;
+
+    // Test mesh
+    // const testMesh = new THREE.Mesh(
+    //   new THREE.BoxGeometry(1, 1, 1),
+    //   new THREE.MeshStandardMaterial()
+    // );
+    // this.scene.add(testMesh);
+
+    this.resources.on('ready', () => {
+      // Setup
+      this.floor = new Floor();
+      this.fox = new Fox();
+      this.environment = new Environment();
+    });
+  }
+  update() {
+    if (this.fox) this.fox.update();
+  }
+}
